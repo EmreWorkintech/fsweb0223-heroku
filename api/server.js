97ac71cc056f39f1e7e4607ba6664ccc650bc494
EmreSahiner <emre@journeyapp.com>
2023-05-18 11:21:08 +0300

    heroku fix

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const server = express();
 
 server.get('/', (req,res)=> {
     console.log('Get request...');
-    res.json({message: "Welcome to mey app!..."})
+    res.json({message: "Welcome to my Heroku app!..."})
 })
 
 
